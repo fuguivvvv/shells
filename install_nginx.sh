a6e0186dcd58540c799a5ecaeffcbf283788ddb8
fuguivvvv <38056259+fuguivvvv@users.noreply.github.com>
2020-02-20 19:58:45 +0800

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,9 +1,9 @@
-yum install yum-utils -y
+install yum-utils -y
 
 cat > /etc/yum.repos.d/nginx.repo  <<EOF
 [nginx-stable]
 name=nginx stable repo
-baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/centos/i\$releasever/\$basearch/
 gpgcheck=1
 enabled=1
 gpgkey=https://nginx.org/keys/nginx_signing.key
@@ -11,7 +11,7 @@ module_hotfixes=true
 
 [nginx-mainline]
 name=nginx mainline repo
-baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
+baseurl=http://nginx.org/packages/mainline/centos/\$releasever/\$basearch/
 gpgcheck=1
 enabled=0
 gpgkey=https://nginx.org/keys/nginx_signing.key

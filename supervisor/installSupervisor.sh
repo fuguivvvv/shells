38c29230cda6510f0a7da4bab0bd2a864b719afb
fuguivvvv <38056259+fuguivvvv@users.noreply.github.com>
2020-04-05 21:52:06 +0800

    Update installSupervisor.sh

--- supervisor/installSupervisor.sh
+++ supervisor/installSupervisor.sh
@@ -1,3 +1,4 @@
+# bash <(curl -L -s https://raw.githubusercontent.com/fuguivvvv/shells/master/supervisor/installSupervisor.sh)
 yum install epel-release
 yum install -y supervisor
 systemctl enable supervisord

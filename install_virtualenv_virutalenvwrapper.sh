33a74f8f823d33c37ba2dd9fd8efa20e96432856
fuguivvvv <38056259+fuguivvvv@users.noreply.github.com>
2021-01-30 22:51:54 +0800

    Update install_virtualenv_virutalenvwrapper.sh

--- install_virtualenv_virutalenvwrapper.sh
+++ install_virtualenv_virutalenvwrapper.sh
@@ -6,7 +6,12 @@ pip virutalenv
 pip virtualenvwrapper
 pip3 virtualenv
 pip3 virtualenvwapper
+
+
+echo PATH=$PATH:/usr/local/python3/bin >> /etc/profile
+echo export PATH >> /etc/profile
 echo export WORKON_HOME=/root/virtualenvs >> /etc/profile
 echo source /usr/local/python3/bin/virtualenvwrapper.sh >> /etc/profile
+
 source /etc/profile
 mkvirtualenv -p python3 test_env3
